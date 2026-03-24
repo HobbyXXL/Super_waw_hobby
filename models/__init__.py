434b3cd503fb38fc298705f37a5394cab07fe0c8
KamuGys <168468140+KamuGys@users.noreply.github.com>
2026-03-24 20:29:30 +0300

    Delete models directory

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .post import Post
-from .user import User
-from .tag import Tag
-from .post_tag import PostTag
-from .like import Like
-from .comment import Comment
--- models/levels.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Level(Base):
-    __tablename__ = "levels"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
-    min_hours = Column(Integer)
