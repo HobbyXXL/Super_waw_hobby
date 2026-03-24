0ca20baa71fe5cfc7fc42e47bd0aca86dc33a828
KamuGys <168468140+KamuGys@users.noreply.github.com>
2026-03-24 20:39:08 +0300

    Add files via upload

--- FASTAPI-MVP/database.py
+++ FASTAPI-MVP/database.py
@@ -1,16 +1,19 @@
-# FASTAPI-MVP/database.py
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import os
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def get_db():
