aabcb3644e2752b426a5356f929e72be3b996703
KamuGys <168468140+KamuGys@users.noreply.github.com>
2026-05-10 10:38:11 +0300

    Delete models directory

--- models/levels.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .base import Base
-
-class Level(Base):
-    __tablename__ = "levels"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
-    min_hours = Column(Integer)
